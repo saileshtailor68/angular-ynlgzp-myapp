ffaf50cc0b5f6f3d89e81c6c6ffba7b9fd5ef79a
saileshtailor68 <saileshtailor@hotmail.co.uk>
2020-01-06 16:44:10 +0000

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { FormsModule } from "@angular/forms";
 
 //Import the module that contains all the material components
 import { AppMaterialModule } from "./app.material.module";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 //aplication modules
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppMaterialModule],
+  imports: [BrowserModule, FormsModule, AppMaterialModule, BrowserAnimationsModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent]
 })
